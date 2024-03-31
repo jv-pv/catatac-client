5d1680e30ff709a73f58ff898f5aeefd3aeb4b1d
John Pieri <jvpieri@gmail.com>
2024-03-31 17:34:14 -0400

    Added custom themes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        aboutFont: ["aboutFont", "serif"],
+      },
+      gridTemplateRows: {
+        'auto': "auto / 1fr",
+        'prints' : "auto / repeat(3, 1fr)"
+      },
+      backgroundImage: {
+        'doodle': "url('/images/doodle.svg')"
+      }
+    },
   },
   plugins: [],
 }
