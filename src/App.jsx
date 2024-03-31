d6f3d77dd393fb9739f94d48e9c5a2285075c49a
John Pieri <jvpieri@gmail.com>
2024-03-30 22:34:49 -0400

    Added new routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,45 +3,41 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
-import './App.css'
-
+import "./App.css";
+import AdminProductFormPage from "./pages/AdminProductFormPage";
 
 function App() {
-
   const getToken = () => {
-    return localStorage.getItem('authToken')
-  }
+    return localStorage.getItem("authToken");
+  };
 
   const LoggedIn = () => {
-    return getToken() ? <Outlet /> : <Navigate to='/login' />
-  }
+    return getToken() ? <Outlet /> : <Navigate to='/login' />;
+  };
 
   const NotLoggedIn = () => {
-    return !getToken() ? <Outlet /> : <Navigate to='/' />
-  }
+    return !getToken() ? <Outlet /> : <Navigate to='/' />;
+  };
 
   return (
-    <div className="main-app-container">
+    <div className='flex flex-col bg-gray-100 text-white w-full min-h-dvh'>
       <Navbar />
 
-      <Routes>      
-        <Route exact path="/" element={<HomePage />} />
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
 
         <Route element={<LoggedIn />}>
-
+          <Route path="/admin/products" element={<AdminProductFormPage />} />
 
         </Route>
 
         <Route element={<NotLoggedIn />}>
-
           <Route path='/signup' element={<SignupPage />} />
           <Route path='/login' element={<LoginPage />} />
-          
         </Route>
-
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
