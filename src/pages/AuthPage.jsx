14a00371b3c6ccd65f622338da59f4a46bed3db1
John Pieri <jvpieri@gmail.com>
2024-03-30 22:36:29 -0400

    Initial Commit

---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,17 @@
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import { useNavigate } from "react-router-dom";
+import { post } from "../services/authService";
+
+const AuthPage = () => {
+
+
+
+
+
+  return (
+    <div>AuthPage</div>
+  )
+}
+
+export default AuthPage
