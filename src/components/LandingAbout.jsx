05f99e7d7cd6de1d78b6615be888e7bfd39f8c06
John Pieri <jvpieri@gmail.com>
2024-03-31 22:32:49 -0400

    Addded animation.

--- src/components/LandingAbout.jsx
+++ src/components/LandingAbout.jsx
@@ -1,33 +1,48 @@
-const LandingAbout = () => (
-  <>
-    <article className='grid grid-auto w-[573px] h-[500px]'>
-      <div className='flex flex-col gap-2 tracking-tight'>
-        <h1 className='text-8xl tracking-tighter leading-tighter'>Welcome to</h1>
-        <h2 className='text-6xl leading-10 indent-2'>Catatac Store</h2>
-        <h3 className='text-3xl leading-loose indent-3'>by Catalina Pieri</h3>
-        <p className='text-xl ml-3 text-justify'>
-          Post-ironic helvetica do, health goth mukbang elit raw denim fashion axe ex selfies woke
-          pour-over. Fam consectetur ut tonx poke, deep v actually wolf kitsch coloring book.
-          Meditation culpa enamel pin artisan banjo pop-up, veniam irony pour-over neutra.
-        </p>
-      </div>
-    </article>
+import { useRef, useEffect } from "react"
 
-    <article className='flex items-center justify-center bg-red-500 w-[573px] h-[744px]'>
-      <div className='flex flex-col items-center justify-evenly w-[520px] h-[700px] text-9xl relative z-10'>
-        <h1>CATATAC</h1>
-        <h1>CATATAC</h1>
-        <h1>CATATAC</h1>
-        <h1>CATATAC</h1>
-        <h1>CATATAC</h1>
-        <img
-          src='/images/cover-img.jpg'
-          alt='cover-img'
-          className='absolute w-[450px] h-[477px] object-contain mb-[13px]'
-        />
-      </div>
-    </article>
-  </>
-);
+const LandingAbout = () => {
 
-export default LandingAbout;
+  const aboutRef = useRef(null)
+  const posterRef = useRef(null)
+
+  useEffect(() => {
+    aboutRef.current.classList.add('animate-slide-from-left')
+    posterRef.current.classList.add('animate-slide-from-right')
+  },[])
+
+  return (
+    <>
+    {/* About */}
+      <article ref={aboutRef} className='grid grid-auto w-[573px] h-[500px]'>
+        <div className='flex flex-col gap-2 tracking-tight'>
+          <h1 className='text-8xl tracking-tighter leading-tighter'>Welcome to</h1>
+          <h2 className='text-6xl leading-10 indent-2'>Catatac Store</h2>
+          <h3 className='text-3xl leading-loose indent-3'>by Catalina Pieri</h3>
+          <p className='text-xl ml-3 text-justify'>
+            Post-ironic helvetica do, health goth mukbang elit raw denim fashion axe ex selfies woke
+            pour-over. Fam consectetur ut tonx poke, deep v actually wolf kitsch coloring book.
+            Meditation culpa enamel pin artisan banjo pop-up, veniam irony pour-over neutra.
+          </p>
+        </div>
+      </article>
+  
+      {/* Poster */}
+      <article ref={posterRef} className='flex items-center justify-center bg-red-500 w-[573px] h-[744px]'>
+        <div className='flex flex-col items-center justify-evenly w-[520px] h-[700px] text-9xl relative z-10'>
+          <h1>CATATAC</h1>
+          <h1>CATATAC</h1>
+          <h1>CATATAC</h1>
+          <h1>CATATAC</h1>
+          <h1>CATATAC</h1>
+          <img
+            src='/images/cover-img.jpg'
+            alt='cover-img'
+            className='absolute w-[450px] h-[477px] object-contain mb-[13px]'
+          />
+        </div>
+      </article>
+    </>
+  )
+}
+
+export default LandingAbout
