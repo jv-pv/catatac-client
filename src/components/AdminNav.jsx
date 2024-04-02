844dfa3cd7f78bc67ffb542d7a603c21409684dc
John Pieri <jvpieri@gmail.com>
2024-04-01 22:54:09 -0400

    Added useEffect and conditional to add a bg color when the user scrolls a certain distance Y. This is done so the admin nav is visible over the product cards.

--- src/components/AdminNav.jsx
+++ src/components/AdminNav.jsx
@@ -1,10 +1,36 @@
+import { useEffect, useRef, useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
-
 const AdminNav = () => {
+
+  const [isScrolled, setIsScrolled] = useState(false)
+  const navRef = useRef(null)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop
+      const navHeight = navRef.current.offsetHeight
+  
+      if (scrollTop > navHeight - 50) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+
+  }, [])
+
+
+
   return (
     <>
-    <div className='grid place-items-center font-headerFont text-black w-full h-20'>
+    <div ref={navRef} className={`grid place-items-center font-headerFont text-black w-full h-16 bg-gray-100 fixed top-16 left-0 right-0 z-10 ${isScrolled ? "bg-gray-100" : "bg-transparent" }`}>
       <ul className='flex justify-evenly items-center w-80'>
         <NavLink to='/admin/products/add'>
           <li>Add Products</li>
