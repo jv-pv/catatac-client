f22f5334c7e8576a8d1509c7575d3040cb63d141
John Pieri <jvpieri@gmail.com>
2024-04-01 22:55:47 -0400

    Added edit form modal that pops up when the edit button is clicked

--- src/components/AdminProductCard.jsx
+++ src/components/AdminProductCard.jsx
@@ -1,6 +1,14 @@
+import { useContext, useState } from "react";
+import EditProduct from "./EditProduct";
 import { axiosDelete } from "../services/authService";
+import EditProductForm from "./EditProductForm";
+import { ProductContext } from "../context/product.context";
 
-const AdminProductCard = ({ product, fetchProducts }) => {
+const AdminProductCard = ({ product }) => {
+
+    const [showModal, setShowModal] = useState(false)
+
+    const { fetchProducts } = useContext(ProductContext)
 
     const handleProductDelete = async () => {
         try {
@@ -32,20 +40,32 @@ const AdminProductCard = ({ product, fetchProducts }) => {
           <div className='flex w-full mt-auto'>
             <div>
               <p className='text-lg inline-block mr-5'>
-                <span className='font-bold'>Price: </span>{product.price}
+                <span className='font-bold'>Price: </span>${product.price}
               </p>
               <p className='text-lg inline-block'>
                 <span className='font-bold'>Stock: </span>{product.stock}
               </p>
             </div>
             <div className='flex items-center justify-end w-full pr-4 gap-3'>
-              <button className="bg-blue-500 text-black w-8 h-8 rounded-full flex items-center justify-center">
+              <button className="bg-blue-500 text-black w-8 h-8 rounded-full flex items-center justify-center" onClick={() => setShowModal(true)}>
                 <img src='/page-edit.svg' alt='' className="w-5 h-5" />
               </button>
               <button className='bg-red-600 text-blue-50 rounded-full w-8 h-8' onClick={handleProductDelete}>X</button>
             </div>
           </div>
         </article>
+
+        <EditProduct 
+        showModal={showModal}
+        closeModal={() => setShowModal(false)}
+        >
+        
+        <EditProductForm
+        product={product}
+        setShowModal={setShowModal}
+        />
+
+        </EditProduct>
       </div>
     );
   };
