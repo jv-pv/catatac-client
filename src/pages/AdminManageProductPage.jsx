700b21b4a8af75e4ee94d77b3974e62526752346
John Pieri <jvpieri@gmail.com>
2024-04-01 20:36:06 -0400

    Minor adjustments, set up cloudinary

--- src/pages/AdminManageProductPage.jsx
+++ src/pages/AdminManageProductPage.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useContext, useState } from "react";
+import { ProductContext } from "../context/product.context";
+import AdminProductCard from "../components/AdminProductCard";
+
 
 const AdminManageProductPage = () => {
-  return (
-    <div>AdminManageProductPage</div>
-  )
-}
 
-export default AdminManageProductPage
+    const [showModal, setShowModal] = useState(false)
+
+    const {products, fetchProducts} = useContext(ProductContext)
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
+
+  return (
+    <section className='flex flex-col items-center gap-5 flex-1'>
+    
+    {products.map((product) => (
+        <AdminProductCard key={product._id} product={product} fetchProducts={fetchProducts} showModal={showModal} setShowModal={setShowModal}/>
+    ))}
+    </section>
+  );
+};
+
+export default AdminManageProductPage;
