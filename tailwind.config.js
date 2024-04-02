65b48eb5982e054b72461fa35a8a4edc35299d90
John Pieri <jvpieri@gmail.com>
2024-04-01 20:33:25 -0400

    Formatted with prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+  "./index.html", 
+  "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       fontFamily: {
-        aboutFont: ["aboutFont", "serif"],
-        headerFont: ["headerFont", "sans-serif"],
+        'aboutFont': ["aboutFont", "serif"],
+        'headerFont': ["headerFont", "sans-serif"],
       },
       gridTemplateRows: {
         'auto': "auto / 1fr",
-        'prints' : "auto / repeat(3, 1fr)"
+        'prints': "auto / repeat(3, 1fr)",
       },
       backgroundImage: {
-        'doodle': "url('/images/doodle.svg')"
+        'doodle': "url('/images/doodle.svg')",
       },
     },
   },
   plugins: [],
-}
+};
