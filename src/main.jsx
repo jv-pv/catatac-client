a80ca0831b9e1afbd89997811051b628033b174f
John Pieri <jvpieri@gmail.com>
2024-04-01 20:34:22 -0400

    Added products context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/auth.context.jsx'
+import { ProductProvider } from './context/product.context.jsx'
 import App from './App.jsx'
 import './index.css'
 
@@ -9,7 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ProductProvider>
+          <App />
+        </ProductProvider>
       </AuthProvider>
     </BrowserRouter>
   // </React.StrictMode>,
