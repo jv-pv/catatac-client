79cd4a69408551c6c284964d21e57af4c28c0c65
John Pieri <jvpieri@gmail.com>
2024-04-02 18:36:08 -0400

    Moved product cards layout to it's own component

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -1,8 +1,15 @@
+import { NavLink } from "react-router-dom";
 
-const ProductCards = () => {
-  return (
-    <div className="text-black">ProductCars</div>
-  )
-}
+const ProductCards = ({product}) => (
+    <NavLink to={`/product/${product._id}`}>
+      <article className='flex flex-col w-full hover:scale-105 transition-transform duration-300 '>
+        <img src={product.imageUrl} alt='test-card' className='object-contain' />
+        <div className='flex justify-between w-full bg-gray-100 px-1'>
+          <span className='text-2xl'>{product.name}</span>
+          <span className='text-2xl'>${product.price}</span>
+        </div>
+      </article>
+    </NavLink>
+  );
 
-export default ProductCards
+export default ProductCards;
