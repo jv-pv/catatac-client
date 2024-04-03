cec40d1de47fded2e4b9f811e8fc1ce70cfda4ca
John Pieri <jvpieri@gmail.com>
2024-04-02 22:59:27 -0400

    Created error page for undefined routes

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div className='flex flex-col flex-1 items-center justify-center text-black'>
+        <h1 className='text-3xl font-headerFont'>Page not found :(</h1>
+    </div>
+  )
+}
+
+export default ErrorPage
