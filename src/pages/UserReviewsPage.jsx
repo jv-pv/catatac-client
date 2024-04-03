8aef2ff0356724b117b17578baeec963c3059328
John Pieri <jvpieri@gmail.com>
2024-04-02 23:01:09 -0400

    Created user reviews page. Still pending.

---
+++ src/pages/UserReviewsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserReviewsPage = () => {
+  return (
+    <div className='flex flex-col flex-1 items-center mt-36 text-black'>
+        UserReviewsPage
+    </div>
+  )
+}
+
+export default UserReviewsPage
