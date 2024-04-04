46fc7885e90ce4c3d3d7bc54847fc9172c690ae9
John Pieri <jvpieri@gmail.com>
2024-04-04 18:39:35 -0400

    Changed to orders page

--- src/pages/UserReviewsPage.jsx
+++ src/pages/UserReviewsPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const UserReviewsPage = () => {
   return (
     <div className='flex flex-col flex-1 items-center mt-36 text-black'>
-        UserReviewsPage
+        No Orders
     </div>
   )
 }
