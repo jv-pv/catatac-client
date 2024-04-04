932086de07d02fc1ac0deb3fb3f8fb9822081f22
John Pieri <jvpieri@gmail.com>
2024-04-03 23:52:21 -0400

    Finished user profile data page. Added condionall rendering for fields.

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -1,50 +1,65 @@
-import { useContext, useEffect, useState } from "react"
-import { get } from "../services/authService"
-import { AuthContext } from "../context/auth.context"
+import { useContext, useEffect, useState } from "react";
+import { get } from "../services/authService";
+import { AuthContext } from "../context/auth.context";
 
 const UserProfilePage = () => {
+  const { user } = useContext(AuthContext);
+  const [thisUser, setThisUser] = useState(null);
 
-    const {user} = useContext(AuthContext)
-    const [thisUser, setThisUser] = useState(null)
-
-    const fetchUser = async () => {
-        try {
-            const response = await get(`/users/profile/${user?._id}`)
-            setThisUser(response.data)
-        } catch (error) {
-            console.error(error.response.data)
-        }
+  const fetchUser = async () => {
+    try {
+      const response = await get(`/users/profile/${user?._id}`);
+      setThisUser(response.data);
+    } catch (error) {
+      console.error(error.response);
     }
+  };
 
-    useEffect(() => {
-        fetchUser()
-    }, [])
-    
+  useEffect(() => {
+    if (user) {
+      fetchUser();
+    }
+  }, []);
 
+  const renderAddressField = (field) => {
+    return field ? <p className='italic'>{field}</p> : null;
+  };
 
   return (
     <section className='flex flex-col flex-1 items-center mt-36 text-black font-headerFont'>
-        <article className='bg-white w-96 text-center p-5'>
-            <h1 className="text-xl">Your Details</h1>
-            <div>
-                <h2>Email: {thisUser?.email}</h2>
-            </div>
-            <div>
-                <h2>Name: {thisUser?.name}</h2>
-            </div>
-            <div>
-                <h2>Address:</h2>
-                <p>{thisUser?.address?.street}</p>
-                <span>{thisUser?.address?.city},</span>
-                <span>{thisUser?.address?.state}</span>
-                <p>{thisUser?.address?.zipCode}</p>
-            </div>
-            <div>
-                <p>{thisUser?.phoneNumber}</p>
-            </div>
-        </article>
+      <article className='flex flex-col justify-center gap-2 bg-red-500 border-2 border-black w-96 p-5'>
+        <h1 className='text-xl font-headerFontBold text-center'>Your Details</h1>
+        <div>
+          <h2>
+            <span className='font-headerFontBold'>Email:</span>{" "}
+            <span className='italic'>{thisUser?.email}</span>
+          </h2>
+        </div>
+        <div>
+          <h2>
+            <span className='font-headerFontBold'>Name:</span>{" "}
+            <span className='italic'>{thisUser?.name}</span>
+          </h2>
+        </div>
+        {thisUser?.address && (
+          <div>
+            <h2 className='font-headerFontBold'>Address:</h2>
+            {renderAddressField(thisUser.address.street)}
+            {thisUser.address.city && thisUser.address.state ? (
+              <span className='flex gap-[2px]'>
+                {thisUser.address.city}, {thisUser.address.state}
+              </span>
+            ) : thisUser.address.city ? (
+              <span>{thisUser.address.city}</span>
+            ) : null}
+            {renderAddressField(thisUser.address.country)}
+            {renderAddressField(thisUser.address.zipCode)}
+            {thisUser?.phoneNumber && <p className='italic'>{thisUser.phoneNumber}</p>}
+          </div>
+        )}
+      </article>
     </section>
-  )
-}
+  );
+};
 
-export default UserProfilePage
+export default UserProfilePage;
