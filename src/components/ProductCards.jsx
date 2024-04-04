f643616c859ef6d549a36a1a68ee6c02f01fcb48
John Pieri <jvpieri@gmail.com>
2024-04-04 18:37:32 -0400

    Adjusted styling

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -2,9 +2,9 @@ import { NavLink } from "react-router-dom";
 
 const ProductCards = ({product}) => (
     <NavLink to={`/product/${product._id}`}>
-      <article className='flex flex-col w-full hover:scale-105 transition-transform duration-300 '>
-        <img src={product.imageUrl} alt='test-card' className='object-contain' />
-        <div className='flex justify-between w-full bg-gray-100 px-1'>
+      <article className='flex flex-col w-full hover:scale-105 transition-transform duration-300'>
+        <img src={product.imageUrl} alt='test-card' className='object-contain rounded-t-sm' />
+        <div className='flex justify-between w-full bg-gray-50 px-1 rounded-b-sm border border-black'>
           <span className='text-2xl'>{product.name}</span>
           <span className='text-2xl'>${product.price}</span>
         </div>
