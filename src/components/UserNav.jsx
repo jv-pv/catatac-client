8733d0b8ca8aa3173c15f930bb35fe364f1624e8
John Pieri <jvpieri@gmail.com>
2024-04-04 15:55:28 -0400

    Added ref to nav so it get a bg color after scrolling a certain distance

--- src/components/UserNav.jsx
+++ src/components/UserNav.jsx
@@ -1,5 +1,5 @@
 import { NavLink, Outlet, useLocation } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { AuthContext } from "../context/auth.context";
 
 const UserNav = () => {
@@ -8,9 +8,32 @@ const UserNav = () => {
   const userEditPage = location.pathname === `/user/edit/${user?._id}`;
   const userReviewsPage = location.pathname === `/user/${user?._id}/reviews`;
 
+  const navRef = useRef(null)
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop
+      const navHeight = navRef.current.offsetHeight
+
+      if (scrollTop > navHeight) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+
+      window.addEventListener('scroll', handleScroll)
+    }
+    
+          return () => {
+            window.removeEventListener('scroll', handleScroll)
+          }
+    
+  }, [])
+
   return (
     <>
-      <div className='grid place-items-center font-headerFont text-black w-full h-16 bg-gray-100 fixed top-16 left-0 right-0 z-10'>
+      <div ref={navRef} className={`grid place-items-center font-headerFont text-black w-full h-16 fixed top-16 left-0 right-0 z-10 ${isScrolled ? "bg-gray-100" : "bg-transparent"}`}>
         <ul className='flex justify-evenly items-center w-80'>
           {userProfilePage && (
             <>
