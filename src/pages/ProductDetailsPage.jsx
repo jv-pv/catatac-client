b562ef72b12b74d8e93280820744fdd986966afd
John Pieri <jvpieri@gmail.com>
2024-04-04 17:03:31 -0400

    Added logic so if user is loged out and tries to add to cart it redirects them to the auth page. If user is logged it adds to cart

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,28 +1,30 @@
-import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { NavLink, useNavigate, useParams } from "react-router-dom";
 import { get, post } from "../services/authService";
+import { AuthContext } from "../context/auth.context";
 
 const ProductDetailsPage = () => {
   const [quantity, setQuantity] = useState(1);
-  const [selectedProduct, setSelectedProduct] = useState(null)
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
-  const { productId } = useParams()
+  const { isLoggedIn } = useContext(AuthContext);
 
-  const navigate = useNavigate()
+  const { productId } = useParams();
+
+  const navigate = useNavigate();
 
   const fetchProductDetails = async () => {
     try {
-      const response = await get(`/products/details/${productId}`)
-      setSelectedProduct(response.data)
+      const response = await get(`/products/details/${productId}`);
+      setSelectedProduct(response.data);
     } catch (error) {
-      navigate(`/products/${productId}`)
+      navigate(`/products/${productId}`);
     }
-  }
+  };
 
   useEffect(() => {
-      fetchProductDetails()
-  }, [])
-
+    fetchProductDetails();
+  }, []);
 
   const handleQuantityIncrement = () => {
     setQuantity((prev) => prev + 1);
@@ -38,43 +40,40 @@ const ProductDetailsPage = () => {
   };
 
   const handleAddToCart = async () => {
-
     const productToAdd = {
       productId: productId,
-      quantity: quantity
-    }
+      quantity: quantity,
+    };
 
     try {
-      const response = await post(`/cart/add`, productToAdd)
-      console.log(response.data)
+      const response = await post(`/cart/add`, productToAdd);
+      console.log(response.data);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   return (
     <section className='details-grid text-black flex-1 font-headerFont'>
       <article className='image'>
-        <div className="border-8 border-black">
-          <img src={selectedProduct?.imageUrl} alt='' className='w-full max-w-[500px] min-w-[400px]' />
+        <div className='border-8 border-black'>
+          <img
+            src={selectedProduct?.imageUrl}
+            alt=''
+            className='w-full max-w-[500px] min-w-[400px]'
+          />
         </div>
       </article>
       <article className='self-center justify-self-center max-w-96'>
         <div className='flex flex-col items-start justify-center gap-6 px-5 py-7 w-full h-full max-w-[400px] min-w-96 min-h-[250px] text-gray-800 bg-red-500 border-2 border-black rounded-sm'>
-            <h2 className='text-2xl ml-2 underline'>
-              {selectedProduct?.name}
-            </h2>
+          <h2 className='text-2xl ml-2 underline'>{selectedProduct?.name}</h2>
           <div>
-            <p className="ml-2">
-              {selectedProduct?.description}
-            </p>
+            <p className='ml-2'>{selectedProduct?.description}</p>
           </div>
 
           <div className='flex w-full mt-10'>
             <div className='flex justify-between'>
-              <p className='text-lg inline-block ml-2'>
-                  ${selectedProduct?.price}
-              </p>
+              <p className='text-lg inline-block ml-2'>${selectedProduct?.price}</p>
             </div>
           </div>
           <div className='flex w-full justify-between items-center'>
@@ -87,9 +86,24 @@ const ProductDetailsPage = () => {
                 <img src='/svg/plus-circle-black.svg' alt='' />
               </button>
             </div>
-            <button type='button' className='bg-black text-white w-32 h-10 self-end hover:text-red-500' onClick={handleAddToCart}>
-              Add to Cart
-            </button>
+            {isLoggedIn ? (
+              <button
+                type='button'
+                className='bg-black text-white w-32 h-10 self-end hover:text-red-500'
+                onClick={handleAddToCart}
+              >
+                Add to Cart
+              </button>
+            ) : (
+              <NavLink to='/login'>
+                <button
+                  type='button'
+                  className='bg-black text-white w-32 h-10 self-end hover:text-red-500'
+                >
+                  Add to Cart
+                </button>
+              </NavLink>
+            )}
           </div>
         </div>
       </article>
