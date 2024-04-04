dd94dc7369e479dd5186fa10b11b47c65c457efd
John Pieri <jvpieri@gmail.com>
2024-04-04 16:52:04 -0400

    Added new url

--- src/services/SERVER_URL.js
+++ src/services/SERVER_URL.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = "http://localhost:4000"
-// export const SERVER_URL = "https://catatac-server.adaptable.app"
+// export const SERVER_URL = "http://localhost:4000"
+export const SERVER_URL = "https://catatac-server.adaptable.app"
