57e3c38f43db58c0f324671deb6a2c4140f317b1
John Pieri <jvpieri@gmail.com>
2024-04-04 19:59:51 -0400

    Added orders endpoint link

--- src/components/AdminNav.jsx
+++ src/components/AdminNav.jsx
@@ -31,13 +31,17 @@ const AdminNav = () => {
   return (
     <>
     <div ref={navRef} className={`grid place-items-center font-headerFont text-black w-full h-16 bg-gray-100 fixed top-16 left-0 right-0 z-10 ${isScrolled ? "bg-gray-100 drop-shadow-md" : "bg-transparent" }`}>
-      <ul className='flex justify-evenly items-center w-80'>
+      <ul className='flex justify-evenly items-center w-96'>
         <NavLink to='/admin/products/add'>
           <li>Add Products</li>
         </NavLink>
         <li className='select-none'>|</li>
         <NavLink to='/admin/products/manage'>
             <li>Manage Products</li>
+        </NavLink>
+        <li className='select-none'>|</li>
+        <NavLink to="/admin/orders">
+            <li>Orders</li>
         </NavLink>
       </ul>
     </div>
