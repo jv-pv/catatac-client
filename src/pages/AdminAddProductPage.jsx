3cf0d162a14430746c209a29c5c70a1b0437a727
John Pieri <jvpieri@gmail.com>
2024-04-04 23:33:05 -0400

    Moved admin add form to it's own component

--- src/pages/AdminAddProductPage.jsx
+++ src/pages/AdminAddProductPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { post } from "../services/authService";
 import { useNavigate } from "react-router-dom";
 import { fileChange } from "../services/imageUpload";
+import AdminAddProductForm from "../components/AdminAddProductForm";
 
 const AdminAddProductPage = () => {
   const [newProduct, setNewProduct] = useState({
@@ -74,68 +75,7 @@ const AdminAddProductPage = () => {
 
   return (
     <section className='text-black w-full flex justify-center flex-1 mt-44 font-headerFont'>
-      <div className='w-96 h-full bg-red-500 p-5 border-2 border-black rounded-md'>
-        <form onSubmit={handleAddSubmit} className='flex flex-col h-full'>
-          <label htmlFor='image-url'>Image:</label>
-          {/* <input type="file" name="" id="" /> */}
-          <input
-            type='file'
-            name='imageUrl'
-            id='image-url'
-            onChange={handleImageUpload}
-            className='my-2 mx-0 px-2 rounded-sm'
-            required
-          />
-          <label htmlFor='product-name'>Name:</label>
-          <input
-            type='text'
-            name='name'
-            id='product-name'
-            value={newProduct.name}
-            onChange={handleFormChange}
-            className='my-2 mx-0 px-1 rounded-sm'
-            required
-          />
-          <label htmlFor='product-description'>Description:</label>
-          <textarea
-            type='text'
-            name='description'
-            id='product-description'
-            value={newProduct.description}
-            onChange={handleFormChange}
-            className='my-2 mx-0 px-1 rounded-sm'
-            required
-          />
-          <label htmlFor='product-price'>Price:</label>
-          <input
-            type='number'
-            name='price'
-            id='product-price'
-            value={newProduct.price}
-            onChange={handleFormChange}
-            className='my-2 mx-0 px-1 rounded-sm'
-            required
-          />
-          <label htmlFor='product-stock'>Stock:</label>
-          <input
-            type='number'
-            name='stock'
-            id='product-stock'
-            value={newProduct.stock}
-            onChange={handleFormChange}
-            className='my-2 mx-0 px-1 rounded-sm'
-            required
-          />
-          {/* <button type='submit' className='mt-6 bg-orange-300 w-24 self-center p-1 rounded-sm hover:bg-gray-900 hover:text-white transition-colors duration-300'>
-            Add
-          </button> */}
-          <button disabled={disabled} type='submit' className='mt-6 bg-orange-300 w-24 self-center p-1 rounded-sm hover:bg-gray-900 hover:text-white transition-colors duration-300'>
-            Add
-          </button>
-        </form>
-        {addProductErrorMsg && <p className="text-center mt-3">{addProductErrorMsg}</p>}
-        {addProductSuccessMsg && <p className="text-center mt-3">{addProductSuccessMsg}</p>}
-      </div>
+      <AdminAddProductForm newProduct={newProduct} handleAddSubmit={handleAddSubmit} handleFormChange={handleFormChange} handleImageUpload={handleImageUpload} disabled={disabled} addProductSuccessMsg={addProductSuccessMsg} addProductErrorMsg={addProductErrorMsg}/>
     </section>
   );
 };
