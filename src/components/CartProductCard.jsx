7dcd3a43cb13261a0018f3b8d1a31bd3c6bde836
John Pieri <jvpieri@gmail.com>
2024-04-04 20:00:21 -0400

    Moved cart product card to its own component

--- src/components/CartProductCard.jsx
+++ src/components/CartProductCard.jsx
@@ -1,14 +1,16 @@
-
-const CartProductCard = () => {
+const CartProductCard = ({ item, removeItemFromCart }) => {
   return (
     <div className='flex border-2 border-black text-black rounded'>
       <div className='flex-2'>
-        <img src='/images/tangle.jpg' alt='' className='w-36' />
+        <img src={item.product.imageUrl} alt={item.product.name} className='w-36' />
       </div>
       <div className='flex flex-col flex-1 w-full p-2'>
-        <h2>Name:</h2>
-        <p>Quantity: 1</p>
-        <p className='mt-auto'>Price: $10</p>
+        <h2>Name: {item.product.name}</h2>
+        <p>Quantity: {item.quantity}</p>
+        <div className='flex items-center justify-between mt-auto'>
+          <p>Price: ${item.product.price}</p>
+          <button onClick={() => removeItemFromCart(item.product._id)}>X</button>
+        </div>
       </div>
     </div>
   );
