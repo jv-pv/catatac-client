d103a207843e6f3fbee4d7e929b7e1267e992ca3
John Pieri <jvpieri@gmail.com>
2024-04-04 23:31:59 -0400

    Added orders pages for user and admin for future. Will be developed later on

--- src/pages/UserOrdersPage.jsx
+++ src/pages/UserOrdersPage.jsx
@@ -1,10 +1,10 @@
 
-const AdminOrdersPage = () => {
+const UserOrdersPage = () => {
   return (
     <div className='flex flex-col flex-1 items-center mt-36 text-black text-2xl font-headerFontBold'>
-         No Orders To Fulfil, Yet
+        No Orders Made
     </div>
   )
 }
 
-export default AdminOrdersPage
+export default UserOrdersPage
